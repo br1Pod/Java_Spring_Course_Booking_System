6d24522f91eefd5d89d18a7ee468cb64c815d113
briPod <33463220+br1Pod@users.noreply.github.com>
2021-10-14 11:11:46 +0100

    booking model written

--- src/main/java/com/codeclan/example/coursebookingsystem/models/Booking.java
+++ src/main/java/com/codeclan/example/coursebookingsystem/models/Booking.java
@@ -1,5 +1,8 @@
 package com.codeclan.example.coursebookingsystem.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
@@ -13,4 +16,54 @@ public class Booking {
     @Column(name = "date")
     private String date;
 
+    @JsonIgnoreProperties({"bookings"})
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @JsonIgnoreProperties({"bookings"})
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    public Booking() {
+    }
+
+    public Booking(String date, Course course, Customer customer) {
+        this.date = date;
+        this.course = course;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }

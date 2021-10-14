159872235a0c13da944a9ac8fa36c751fc4c06b3
briPod <33463220+br1Pod@users.noreply.github.com>
2021-10-14 13:45:08 +0100

    controllers added for findById

--- src/main/java/com/codeclan/example/coursebookingsystem/controllers/BookingController.java
+++ src/main/java/com/codeclan/example/coursebookingsystem/controllers/BookingController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ class BookingController {
     @GetMapping(value = "/bookings")
     public ResponseEntity<List<Booking>> getAllBookings(){
         return new ResponseEntity<>(bookingRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/bookings/{id}")
+    public ResponseEntity getBooking(@PathVariable Long id){
+        return new ResponseEntity<>(bookingRepository.findById(id), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/codeclan/example/coursebookingsystem/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/coursebookingsystem/controllers/CourseController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +21,11 @@ class CourseController {
     @GetMapping(value = "/courses")
     public ResponseEntity<List<Course>> getAllCourses(){
         return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/courses/{id}")
+    public ResponseEntity getCourse(@PathVariable Long id){
+        return new ResponseEntity<>(courseRepository.findById(id), HttpStatus.OK);
     }
 
 
